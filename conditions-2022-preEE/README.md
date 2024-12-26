39a16304e6cf9e872b7fba00bcc94789a9a9875f
LukaLambrecht <Luka.Lambrecht@UGent.be>
2024-12-26 17:59:53 +0100

    add conditions for other years

---
+++ conditions-2022-preEE/README.md
@@ -0,0 +1,10 @@
+# Processing sequence with 2022-preEE conditions
+
+The recommended processing sequence, correct global tags, etc., can be retrieved from [this PdmV TWiki](https://twiki.cern.ch/twiki/bin/view/CMS/PdmVRun3Analysis#Recipes_for_Run3Summer22_and_Run)
+
+The sequence here corresponds to MiniAOD v4 and NanoAOD v12.
+
+Customizations:
+- Monitoring in the form of `--customise Configuration/DataProcessing/Utils.addMonitoring` was added to every step.
+- The random number seed is set as follows (in the LHE/GEN-SIM step): `--customise_commands process.RandomNumberGeneratorService.externalLHEProducer.initialSeed="int(${SEED})"\\nprocess.source.firstLuminosityBlock="cms.untracked.uint32(${JOBNUM})"\\nprocess.source.numberEventsInLuminosityBlock="cms.untracked.uint32(${EVENTS})"`
+
---
+++ conditions-2022-preEE/nanoaod_run.sh
@@ -0,0 +1,172 @@
+#!/bin/bash
+
+# parse command line args
+# (job number and number of events)
+
+JOBNUM=$1
+if [ -z $JOBNUM ]; then
+    echo "Please provide a job number"
+    exit 1
+fi
+
+EVENTS="${2#events=}"
+if [ -z $EVENTS ]; then
+    EVENTS=10
+    echo "Using default number of events: $EVENTS"
+fi
+
+# store current working directory for later use
+BASE=$PWD
+
+# set the random seed
+SEED=$(($(date +%s) % 100 + $JOBNUM))
+
+# define auxiliary functions
+
+build_cmssw() {
+    # install a specified CMSSW version
+
+    VERSION=$1
+
+    echo "================= CMSRUN setting up CMSSW_$VERSION ===================="
+    if [ -r CMSSW_$VERSION/src ] ; then 
+      echo release CMSSW_$VERSION already exists
+    
+      cd CMSSW_$VERSION/src
+      eval `scram runtime -sh`
+
+      cd $BASE
+    
+    else
+      scram p CMSSW CMSSW_$VERSION
+
+      if [ ! -f CMSSW_$VERSION/src/Configuration ]; then
+          cp -r Configuration CMSSW_$VERSION/src
+      fi
+    
+      cd CMSSW_$VERSION/src
+      eval `scram runtime -sh`
+      scram b
+
+      cd $BASE
+    fi
+}
+
+run() {
+    # build and run a specified configuration file with cmsDriver and cmsRun
+
+    export SCRIPT=$1
+    REPORT_NAME=FrameworkJobReport.xml
+    shift 1
+
+    export ARGS=$@
+
+    echo "================= CMSRUN setting up $SCRIPT step ===================="
+
+    cmsDriver.py $ARGS --python_filename $SCRIPT.py -n $EVENTS || exit $? ;
+
+    echo "================= CMSRUN starting $SCRIPT step ===================="
+
+    cmsRun -e -j $REPORT_NAME $SCRIPT.py
+    status=$?
+
+    if [ $status -ne 0 ]; then
+        echo "================= CMSRUN error with exit status $status ===================="
+        rm -f *.root
+        exit $status
+    fi
+
+}
+
+# main
+
+echo "================= CMSRUN starting jobNum=$1 ===================="
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+# LHE and GEN-SIM step
+export SCRAM_ARCH=el8_amd64_gcc10
+build_cmssw 12_4_14_patch3
+run 0_HIG-Run3Summer22wmLHEGS \
+    Configuration/GenProduction/python/fragment.py \
+    --fileout "file:0_HIG-Run3Summer22wmLHEGS.root" \
+    --eventcontent RAWSIM,LHE \
+    --customise Configuration/DataProcessing/Utils.addMonitoring \
+    --datatier GEN-SIM,LHE \
+    --conditions 124X_mcRun3_2022_realistic_v12 \
+    --beamspot Realistic25ns13p6TeVEarly2022Collision \
+    --customise_commands process.RandomNumberGeneratorService.externalLHEProducer.initialSeed="int(${SEED})"\\nprocess.source.firstLuminosityBlock="cms.untracked.uint32(${JOBNUM})"\\nprocess.source.numberEventsInLuminosityBlock="cms.untracked.uint32(${EVENTS})" \
+    --step LHE,GEN,SIM \
+    --geometry DB:Extended \
+    --era Run3 \
+    --no_exec \
+    --mc || exit $? ;
+
+# premixing step
+export SCRAM_ARCH=el8_amd64_gcc10
+build_cmssw 12_4_14_patch3
+run 1_HIG-Run3Summer22DRPremix \
+    --filein "file:0_HIG-Run3Summer22wmLHEGS.root" \
+    --fileout "file:1_HIG-Run3Summer22DRPremix.root" \
+    --eventcontent PREMIXRAW \
+    --customise Configuration/DataProcessing/Utils.addMonitoring \
+    --datatier GEN-SIM-RAW \
+    --pileup_input "dbs:/Neutrino_E-10_gun/Run3Summer21PrePremix-Summer22_124X_mcRun3_2022_realistic_v11-v2/PREMIX" \
+    --conditions 124X_mcRun3_2022_realistic_v12 \
+    --step DIGI,DATAMIX,L1,DIGI2RAW,HLT:2022v12 \
+    --procModifiers premix_stage2,siPixelQualityRawToDigi \
+    --geometry DB:Extended \
+    --datamix PreMix \
+    --era Run3 \
+    --no_exec \
+    --mc || exit $? ;
+
+# AOD step
+export SCRAM_ARCH=el8_amd64_gcc10
+build_cmssw 12_4_14_patch3
+run 2_HIG-Run3Summer22AOD \
+    --filein "file:1_HIG-Run3Summer22DRPremix.root" \
+    --fileout "file:2_HIG-Run3Summer22AOD.root" \
+    --eventcontent AODSIM \
+    --customise Configuration/DataProcessing/Utils.addMonitoring \
+    --datatier AODSIM \
+    --conditions 124X_mcRun3_2022_realistic_v12 \
+    --step RAW2DIGI,L1Reco,RECO,RECOSIM \
+    --procModifiers siPixelQualityRawToDigi \
+    --geometry DB:Extended \
+    --era Run3 \
+    --no_exec \
+    --mc || exit $? ;
+
+# MiniAOD step
+export SCRAM_ARCH=el8_amd64_gcc11
+build_cmssw 13_0_13
+run 3_HIG-Run3Summer22MiniAODv4 \
+    --filein "file:2_HIG-Run3Summer22AOD.root" \
+    --fileout "file:3_HIG-Run3Summer22MiniAODv4.root" \
+    --eventcontent MINIAODSIM \
+    --customise Configuration/DataProcessing/Utils.addMonitoring \
+    --datatier MINIAODSIM \
+    --conditions 130X_mcRun3_2022_realistic_v5 \
+    --step PAT \
+    --geometry DB:Extended \
+    --era Run3,run3_miniAOD_12X \
+    --no_exec \
+    --mc || exit $? ;
+
+# NanoAOD step
+export SCRAM_ARCH=el8_amd64_gcc11
+build_cmssw 13_0_13
+run 4_HIG-Run3Summer22NanoAODv12 \
+    --filein "file:3_HIG-Run3Summer22MiniAODv4.root" \
+    --fileout "file:ntuple.root" \
+    --eventcontent NANOAODSIM \
+    --customise Configuration/DataProcessing/Utils.addMonitoring \
+    --datatier NANOAODSIM \
+    --conditions 130X_mcRun3_2022_realistic_v5 \
+    --step NANO \
+    --scenario pp \
+    --era Run3 \
+    --no_exec \
+    --mc || exit $? ;
+
+echo "================= CMSRUN finished ===================="
