3b952c0b5a5001bb75516df03d4f4c54b64d543f
LukaLambrecht <Luka.Lambrecht@UGent.be>
2025-01-04 22:44:31 +0100

    fix jobcheck to python3

--- jobtools/jobcheck.py
+++ jobtools/jobcheck.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 ################################################
 # Check the error and output log files of jobs #
@@ -33,10 +33,10 @@ import glob
 
 
 def check_start_done( filename, 
-		      starting_tag='###starting###',
-		      done_tag='###done###',
-		      ntarget=None,
-		      verbose=True ):
+                      starting_tag='###starting###',
+                      done_tag='###done###',
+                      ntarget=None,
+                      verbose=True ):
     ### check starting and done tags in a file.
     # returns 0 in case of no errors, which is defined as:
     #   - at least one starting tag is present in the file.
@@ -52,10 +52,10 @@ def check_start_done( filename,
     # count number of starting tags
     nstarted = filetext.count(starting_tag)
     if(nstarted==0):
-	if verbose:
-	    msg = 'WARNING in jobCheck.py: file {}'.format(filename)
-	    msg += ' does not contain a valid starting tag "{}"'.format(starting_tag)
-	    print(msg)
+        if verbose:
+            msg = 'WARNING in jobCheck.py: file {}'.format(filename)
+            msg += ' does not contain a valid starting tag "{}"'.format(starting_tag)
+            print(msg)
         return 1
 
     # count number of done tags
@@ -67,11 +67,11 @@ def check_start_done( filename,
 
     # return 1 otherwise
     if verbose:
-	msg = 'WARNING in jobCheck.py: found issue in file {}:\n'.format(filename)
-	msg += '   {} commands were initiated.\n'.format(nstarted)
-	msg += '   {} seem to have finished normally.\n'.format(ndone)
-	msg += '   {} were expected.\n'.format(ntarget)
-	print(msg)
+        msg = 'WARNING in jobCheck.py: found issue in file {}:\n'.format(filename)
+        msg += '   {} commands were initiated.\n'.format(nstarted)
+        msg += '   {} seem to have finished normally.\n'.format(ndone)
+        msg += '   {} were expected.\n'.format(ntarget)
+        print(msg)
     return 1
 
 
@@ -87,26 +87,26 @@ def check_error_content(filename, contentlist='default', verbose=True):
 
     # hard-coded default error content
     if( isinstance(contentlist,str) and contentlist=='default' ):
-	contentlist = ([    'SysError',
+        contentlist = ([    'SysError',
                            '/var/torque/mom_priv/jobs',
                            'R__unzip: error',
                            'hadd exiting due to error in',
                            'Bus error',
                            'Exception:',
                            'Traceback (most recent call last):' ])
-	contentlist.append('###error###') # custom error tag for targeted flagging
+        contentlist.append('###error###') # custom error tag for targeted flagging
 
     # check if the file content contains provided error tags
     contains = []
     for idx,content in enumerate(contentlist):
-	if filetext.count(content)>0:
-	    contains.append(idx)
+        if filetext.count(content)>0:
+            contains.append(idx)
     if len(contains)==0: return 0
     if verbose:
-	msg = 'WARNING in jobCheck.py: found issue in file {}:\n'.format(filename)
-	for idx in contains:
-	    msg += '   found sequence {}\n'.format(contentlist[idx])
-	print(msg)
+        msg = 'WARNING in jobCheck.py: found issue in file {}:\n'.format(filename)
+        for idx in contains:
+            msg += '   found sequence {}\n'.format(contentlist[idx])
+        print(msg)
     return 1
 
 
@@ -115,23 +115,23 @@ if __name__=='__main__':
     # parse command line arguments
     parser = argparse.ArgumentParser(description='Check job logs for errors.')
     parser.add_argument('--dir', default=os.getcwd(),
-			help='Directory to scan for files (default: cwd)')
+                        help='Directory to scan for files (default: cwd)')
     parser.add_argument('--starting_tag', default='###starting###',
-			help='Starting tag, default is "###starting###".')
+                        help='Starting tag, default is "###starting###".')
     parser.add_argument('--done_tag', default='###done###',
-			help='Done tag, default is "###done###".')
+                        help='Done tag, default is "###done###".')
     parser.add_argument('--ntags', default=None,
-			 help='Number of expected starting and done tags per job.')
+                         help='Number of expected starting and done tags per job.')
     parser.add_argument('--notags', action='store_true',
-			help='Ignore starting and done tags, only check for errors.')
+                        help='Ignore starting and done tags, only check for errors.')
     parser.add_argument('--noerrors', action='store_true',
-			help='Ignore errors, only check starting and done tags.')
+                        help='Ignore errors, only check starting and done tags.')
     args = parser.parse_args()
 
     # print arguments
     print('running with following configuration:')
     for arg in vars(args):
-	print('  - {}: {}'.format(arg,getattr(args,arg)))
+        print('  - {}: {}'.format(arg,getattr(args,arg)))
 
     # some more parsing
     if args.ntags is not None: args.ntags = int(args.ntags)
@@ -148,18 +148,18 @@ if __name__=='__main__':
     # loop over files
     nerror = 0
     for fname in files:
-	# initialize
-	error_start_done = 0
-	error_content = 0
-	# error checking
-	if not args.notags: 
-	    error_start_done = check_start_done(fname,
-		starting_tag = args.starting_tag,
-		done_tag = args.done_tag,
-		ntarget = args.ntags)
-	if not args.noerrors: 
-	    error_content = check_error_content(fname)
-	if(error_start_done + error_content > 0): nerror += 1
+        # initialize
+        error_start_done = 0
+        error_content = 0
+        # error checking
+        if not args.notags: 
+            error_start_done = check_start_done(fname,
+                starting_tag = args.starting_tag,
+                done_tag = args.done_tag,
+                ntarget = args.ntags)
+        if not args.noerrors: 
+            error_content = check_error_content(fname)
+        if(error_start_done + error_content > 0): nerror += 1
 
     # print results
     print('number of files scanned: {}'.format(nfiles))
