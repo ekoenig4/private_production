abd66f56a59a1bcac7f808bb35724ed627b349b2
LukaLambrecht <Luka.Lambrecht@UGent.be>
2025-01-16 15:29:00 +0100

    small improvments in CRAB handling

--- monitoring/crab_kill.py
+++ monitoring/crab_kill.py
@@ -6,6 +6,7 @@
 
 import os
 import sys
+import six
 import glob
 import argparse
 
@@ -14,21 +15,39 @@ if __name__ == '__main__':
 
     # read command line arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--simpackdir', required=True, type=os.path.abspath,
-      help='Simpack directory')
+    parser.add_argument('-i', '--simpackdir', default=None,
+      help='Simpack directory (kill CRAB jobs for all simpacks in this directory)')
+    parser.add_argument('-s', '--simpacks', default=[], nargs='+',
+      help='Simpacks (alternative to --simpackdir; kill CRAB jobs for all provided simpacks)')
     args = parser.parse_args()
 
     # check command line arguments
-    if not os.path.isdir(args.simpackdir):
-        raise Exception('ERROR: simpack directory {} does not exist'.format(args.simpackdir))
+    if args.simpackdir is not None:
+        args.simpackdir = os.path.abspath(args.simpackdir)
+        if not os.path.isdir(args.simpackdir):
+            raise Exception('ERROR: simpack directory {} does not exist'.format(args.simpackdir))
 
     # find all sample folders
     # (note: this is any folder inside a 'crab_logs' folder somewhere inside the simpack dir)
-    fproc = sorted(glob.glob(os.path.join(args.simpackdir,'**/crab_logs/*'), recursive=True))
+    fproc = []
+    if args.simpackdir is not None:
+        fproc += sorted(glob.glob(os.path.join(args.simpackdir,'**/crab_logs/*'), recursive=True))
+    if len(args.simpacks) > 0:
+        for simpack in args.simpacks:
+            simpack = os.path.abspath(simpack)
+            fproc += glob.glob(os.path.join(simpack,'**/crab_logs/*'), recursive=True)
+    fproc = sorted(fproc)
     nfproc = len(fproc)
     if nfproc == 0:
         msg = 'No samples found in provided simpack dir; exiting.'
         raise Exception(msg)
+
+    # printouts
+    print('Will kill the following crab jobs:')
+    for f in fproc: print(f'  - {f}')
+    print('Continue? (y/n)')
+    go = six.moves.input()
+    if not go=='y': sys.exit()
 
     # find the crab_command script for each sample
     # (note: cannot use standard 'crab kill' command since a special container is needed)
--- templates/crab_command.sh
+++ templates/crab_command.sh
@@ -3,10 +3,10 @@
 export SCRAM_ARCH=el8_amd64_gcc11
 export CMSSW_VERSION=CMSSW_13_0_13
 
-pushd $CMSSW_VERSION/src
+pushd $CMSSW_VERSION/src > /dev/null
 eval `scram runtime -sh`
-popd
+popd > /dev/null
 
-source /cvmfs/cms.cern.ch/crab3/crab.sh
+source /cvmfs/cms.cern.ch/crab3/crab.sh prod
 
 crab $@
